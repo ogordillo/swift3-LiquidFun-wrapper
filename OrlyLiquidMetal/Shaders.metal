39d8dc1e7a4c5510a16ac5af6c1b864d7c5351b7
Orlando Gordillo <OrlyGordo@Orlandos-Mac.local>
2017-05-15 02:44:59 -0700

    reached render()

---
+++ OrlyLiquidMetal/Shaders.metal
@@ -0,0 +1,42 @@
+//
+//  Shaders.metal
+//  OrlyLiquidMetal
+//
+//  Created by Orlando Gordillo on 5/15/17.
+//  Copyright © 2017 Orlando Gordillo. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+struct VertexOut {
+    float4 position [[position]];
+    float pointSize [[point_size]];
+};
+
+struct Uniforms {
+    float4x4 ndcMatrix;
+    float ptmRatio;
+    float pointSize;
+};
+
+vertex VertexOut particle_vertex(const device packed_float2* vertex_array [[buffer(0)]],
+                                 const device Uniforms& uniforms [[buffer(1)]],
+                                 unsigned int vid [[vertex_id]]) {
+    VertexOut vertexOut;
+    float2 position = vertex_array[vid];
+    vertexOut.position =
+    uniforms.ndcMatrix * float4(position.x * uniforms.ptmRatio, position.y * uniforms.ptmRatio, 0, 1);
+    vertexOut.pointSize = uniforms.pointSize;
+    
+    vertexOut.position = uniforms.ndcMatrix * float4(position.x * uniforms.ptmRatio, position.y * uniforms.ptmRatio, 0, 1);
+    
+    return vertexOut;
+    
+}
+
+fragment half4 basic_fragment() {
+    return half4(1.0);
+}
+
+
--- OrlyLiquidMetal/ViewController.swift
+++ OrlyLiquidMetal/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
     let particleRadius: Float = 9
     var particleSystem: UnsafeMutableRawPointer!
     
+    var device: MTLDevice! = nil
+    var metalLayer: CAMetalLayer! = nil
+    
+    var particleCount: Int = 0
+    var vertexBuffer: MTLBuffer! = nil
+    
+    var uniformBuffer: MTLBuffer! = nil
+    
+    var pipelineState: MTLRenderPipelineState! = nil
+    var commandQueue: MTLCommandQueue! = nil
+    
+    
     override func viewDidLoad() {
         LiquidFun.createWorld(withGravity: Vector2D(x: 0, y:-gravity))
         super.viewDidLoad()
@@ -32,7 +44,12 @@ class ViewController: UIViewController {
                                              position: Vector2D(x: screenWidth * 0.5 / ptmRatio, y: screenHeight * 0.5 / ptmRatio),
                                              size: Size2D(width: 50 / ptmRatio, height: 50 / ptmRatio))
         
-        printParticleInfo()
+        createMetalLayer()
+        refreshVertexBuffer()
+        refreshUniformBuffer()
+        buildRenderPipeline()
+        render()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,6 +68,121 @@ class ViewController: UIViewController {
             print("particle: \(i) position: (\(position.x), \(position.y))")
         }
     }
+    
+    func createMetalLayer() {
+        device = MTLCreateSystemDefaultDevice()
+        
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
+    }
+    
+    func refreshVertexBuffer () {
+        particleCount = Int(LiquidFun.particleCount(forSystem: particleSystem))
+        let positions = LiquidFun.particlePositions(forSystem: particleSystem)
+        let bufferSize = MemoryLayout<Float>.size * particleCount * 2
+        vertexBuffer = device.makeBuffer(bytes: positions!, length: bufferSize, options: [])
+    }
+    
+    func makeOrthographicMatrix(left: Float, right: Float, bottom: Float, top: Float, near: Float, far: Float) -> [Float] {
+        let ral = right + left
+        let rsl = right - left
+        let tab = top + bottom
+        let tsb = top - bottom
+        let fan = far + near
+        let fsn = far - near
+        
+        return [2.0 / rsl, 0.0, 0.0, 0.0,
+                0.0, 2.0 / tsb, 0.0, 0.0,
+                0.0, 0.0, -2.0 / fsn, 0.0,
+                -ral / rsl, -tab / tsb, -fan / fsn, 1.0]
+    }
+    
+    func refreshUniformBuffer () {
+        // 1
+        let screenSize: CGSize = UIScreen.main.bounds.size
+        let screenWidth = Float(screenSize.width)
+        let screenHeight = Float(screenSize.height)
+        let ndcMatrix = makeOrthographicMatrix(left: 0, right: screenWidth,
+                                               bottom: 0, top: screenHeight,
+                                               near: -1, far: 1)
+        var radius = particleRadius
+        var ratio = ptmRatio
+        
+        // 2
+        let floatSize = MemoryLayout<Float>.size
+        let float4x4ByteAlignment = floatSize * 4
+        let float4x4Size = floatSize * 16
+        let paddingBytesSize = float4x4ByteAlignment - floatSize * 2
+        let uniformsStructSize = float4x4Size + floatSize * 2 + paddingBytesSize
+        
+        // 3
+        uniformBuffer = device.makeBuffer(length: uniformsStructSize, options: [])
+        let bufferPointer = uniformBuffer.contents()
+        memcpy(bufferPointer, ndcMatrix, float4x4Size)
+        memcpy(bufferPointer + float4x4Size, &ratio, floatSize)
+        memcpy(bufferPointer + float4x4Size + floatSize, &radius, floatSize)
+    }
+    
+    func buildRenderPipeline() {
+        // 1
+        let defaultLibrary = device.newDefaultLibrary()
+        let fragmentProgram = defaultLibrary?.makeFunction(name: "basic_fragment")
+        let vertexProgram = defaultLibrary?.makeFunction(name: "particle_vertex")
+        
+        // 2
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.vertexFunction = vertexProgram
+        pipelineDescriptor.fragmentFunction = fragmentProgram
+        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        
+
+        var pipelineError  = Error?.self
+        
+        do{
+            try pipelineState = device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        }catch  is Error{
+            print("error line 145 in viewcontroller")
+        }
+        
+        if (pipelineState == nil) {
+            print("Error occurred when creating render pipeline state: \(pipelineError)");
+        }
+        
+        // 3
+        commandQueue = device.makeCommandQueue()
+    }
+    
+    func render() {
+        var drawable = metalLayer.nextDrawable()
+        
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable?.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+        renderPassDescriptor.colorAttachments[0].storeAction = .store
+        renderPassDescriptor.colorAttachments[0].clearColor =
+            MTLClearColor(red: 0.0, green: 104.0/255.0, blue: 5.0/255.0, alpha: 1.0)
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        
+       
+            renderEncoder.setRenderPipelineState(pipelineState)
+            renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, at: 0)
+            renderEncoder.setVertexBuffer(uniformBuffer, offset: 0, at: 1)
+            
+            renderEncoder.drawPrimitives(type: .point, vertexStart: 0, vertexCount: particleCount, instanceCount: 1)
+            renderEncoder.endEncoding()
+        
+        
+        commandBuffer.present(drawable!)
+        commandBuffer.commit()
+    }
+    
+    
 
 
 }
